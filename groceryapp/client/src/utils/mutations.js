9f5ea9637183af4f18b3ac671f66f42f63333c0c
Hayden Lugo <hayden@lugowebdevelopment.com>
2022-01-13 20:25:29 -0600

    Initial file structure

---
+++ groceryapp/client/src/utils/mutations.js
@@ -0,0 +1,27 @@
+import gql from "graphql-tag";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+      token
+      user {
+        username
+        _id
+        email
+      }
+    }
+  }
+`;
+
